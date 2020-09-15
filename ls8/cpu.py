63cae75c690508190fd1b38cfef940c3054d3b9b
hera <a.heraimenka@gmail.com>
2020-09-14 21:25:55 -0700

    Steps 0-6

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,50 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256    # RAM storage
+        self.pc = 0             # program counter
+        self.ir = 0             # instruction register
+        self.reg = [0] * 8
+        self.working = False    # cpu is turned off by default
+
+        self.instruction_table = {
+            0b10000010: self.LDI,
+            0b00000001: self.HLT,
+            0b01000111: self.PRN
+        }
+    
+    # Set the value of a register to an integer
+    def LDI(self):
+        self.pc += 1
+        location = self.ram_read(self.pc)
+
+        self.pc += 1
+        data = self.ram_read(self.pc)
+
+        self.ram_write(location, data)
+
+    # Halt the CPU
+    def HLT(self):
+        self.working = False
+
+
+    # Print numeric value stored in the given register
+    def PRN(self):
+        self.pc += 1
+
+        location = self.ram_read(self.pc)
+        data = self.ram_read(location)
+
+        print(data)
+
+
+    def ram_read(self, address):
+        return self.ram[address]
+    
+
+    def ram_write(self, address, data):
+        self.ram[address] = data
+
 
     def load(self):
         """Load a program into memory."""
@@ -40,6 +83,7 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
 
+
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -60,6 +104,21 @@ class CPU:
 
         print()
 
+
     def run(self):
         """Run the CPU."""
-        pass
+        
+        self.working = True
+
+        while self.working:
+            # save a copy of the currently executing instruction into the instruction register (IR)
+            self.ir = self.ram_read(self.pc)
+
+            # check if the cpu supports this operation
+            if self.ir in self.instruction_table:
+                # call the handler
+                self.instruction_table[self.ir]()
+
+                self.pc += 1
+            else:
+                raise Exception(f"Unsupported operation: {self.ir}")
